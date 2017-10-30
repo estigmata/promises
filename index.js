52b57878c24f9ccca686d2c64fb00340691f467b
Nestor Otondo <nestor.otondo@gmail.com>
2017-10-29 22:11:19 -0400

    Hope

--- index.js
+++ index.js
@@ -10,4 +10,31 @@ function wait(seconds) {
   });
 }
 
-promiseMap([1, 2, 3], wait).then(console.log);
+function sum(a, b) {
+  return a + b
+};
+
+//promiseMap([1, 2, 3, 'Hola ', 'Nestor ', 'Puedes?', sum(4, 5)], wait).then(console.log);
+
+function doFirstThing(){ return Promise.resolve(1); }
+function doSecondThing(res){ return Promise.resolve(res + 1); }
+function doThirdThing(res){ return Promise.resolve(res + 2); }
+function lastThing(res){ console.log("result:", res); }
+
+var fnlist = [ doFirstThing, doSecondThing, doThirdThing, lastThing];
+
+function pseries(list) {
+  var p = Promise.resolve();
+  return list.reduce(function(pacc, fn) {
+    return pacc = pacc.then(fn);
+  }, p);
+}
+
+pseries(fnlist);
+
+function promiseAll(items) {
+  const execute = (item) => item.then(executeItems => executeItems.concat([executeItems]));
+  return items.reduce(execute, Promise.resolve([]));
+}
+
+promiseAll([1, 2, 3, 'Hola ', 'Nestor ', 'Puedes?', sum(4, 5)], wait).then(console.log);
