0005803582b198d8439f40e16d66be7be4ab6886
Nestor Otondo <nestor.otondo@gmail.com>
2017-10-30 22:52:10 -0400

    Resolve with reduce and mapper

--- index.js
+++ index.js
@@ -1,21 +1,3 @@
-function promiseMap(items, mapper) {
-  const reducer = (promise, item) =>
-    promise.then(mappedItems => mapper(item).then(res => mappedItems.concat([res])));
-  return items.reduce(reducer, Promise.resolve([]));
-}
-
-function wait(seconds) {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => resolve(seconds), seconds * 1000);
-  });
-}
-
-function sum(a, b) {
-  return a + b
-};
-
-//promiseMap([1, 2, 3, 'Hola ', 'Nestor ', 'Puedes?', sum(4, 5)], wait).then(console.log);
-
 function doFirstThing(){ return Promise.resolve(1); }
 function doSecondThing(res){ return Promise.resolve(res + 1); }
 function doThirdThing(res){ return Promise.resolve(res + 2); }
@@ -32,9 +14,19 @@ function pseries(list) {
 
 pseries(fnlist);
 
-function promiseAll(items) {
-  const execute = (item) => item.then(executeItems => executeItems.concat([executeItems]));
-  return items.reduce(execute, Promise.resolve([]));
+function wait(seconds) {
+  return new Promise((resolve, reject) => setTimeout(() => resolve(seconds), seconds * 1000));
 }
 
-promiseAll([1, 2, 3, 'Hola ', 'Nestor ', 'Puedes?', sum(4, 5)], wait).then(console.log);
+function sum(a, b) {
+  return a + b
+};
+
+function promiseAll(items, mapper) {
+  return items.
+    reduce((execute, item) => execute.
+      then(mappedItems => mapper(item).
+        then(res => mappedItems.concat([item]))), Promise.resolve([]));
+}
+
+promiseAll([sum(1, 2), sum(3, 8), sum(4, 5)], wait).then(console.log);
